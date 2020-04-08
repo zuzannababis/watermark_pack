77d9e6df5dd22f2e5a967fbd81332008a02bdad8
Zuza Babis <zuzanna.babis@gmail.com>
2020-04-08 21:22:25 +0200

    Add watermark in the middle

---
+++ app.js
@@ -0,0 +1,15 @@
+const Jimp = require('jimp');
+
+const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
+  const image = await Jimp.read(inputFile);
+  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+  const textData = {
+    text,
+    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+    alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+  };
+  image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
+  await image.quality(100).writeAsync(outputFile);
+};
+
+addTextWatermarkToImage('./test.jpg', './test-with-watermark.jpg', 'Hello world')
